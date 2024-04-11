0a47a593d18020998c73da6001d7a0d908ddac27
Shreya Gubbi <sreenukoondrapu@yahoo.com>
2024-04-11 16:13:18 +0530

    modify terraform file for asp

--- Terraform/webapp.tf
+++ Terraform/webapp.tf
@@ -33,7 +33,7 @@ resource "azurerm_service_plan" "webapp-asp" {
   location            = azurerm_resource_group.devops-webapp-rg.location
   resource_group_name = azurerm_resource_group.devops-webapp-rg.name
   os_type             = "Linux"
-  sku_name            = "F1"
+  sku_name            = "B1"
 }
 #Create Azure App Service
 resource "azurerm_app_service" "devops-webapplication-asp" {
